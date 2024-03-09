43457b7ac805cb0dd0d219a32775204b77529c21
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-03-09 15:01:02 +0900

    Import and modify example.go from github.com/hymkor/go-minimal-optional

---
+++ example.go
@@ -0,0 +1,37 @@
+//go:build ignore
+
+package main
+
+import (
+	"github.com/hymkor/go-tiny-optional"
+)
+
+func test(x optional.Value[int]) {
+	x.IfSome(func(v int) {
+		println("   IfSome: it has a value:", v)
+	})
+
+	for v := range x.Each {
+		println("   for-range(v1.22 X:rangefunc): it has a value:", v)
+	}
+
+	if x.IsNone() {
+		println("   IsNone: it does not have a value")
+	}
+
+	x.Match(func(v int) {
+		println("   Match: it has a value:", v)
+	}, func() {
+		println("   Match: it does not hava a value")
+	})
+
+	println()
+}
+
+func main() {
+	println("None[int]")
+	test(optional.None[int]())
+
+	println("Some(4)")
+	test(optional.Some(4))
+}
